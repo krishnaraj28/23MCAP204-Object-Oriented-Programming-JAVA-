5a95acf6a3c5471fb28740cab7aa69d1bed8a27d
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:05:07 +0530

    Update Fibonacci.java

--- Java Basics/Fibonacci.java
+++ Java Basics/Fibonacci.java
@@ -3,14 +3,14 @@ public class Fibonacci{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the limit: ");
-        int limit = sc.nextInt();
-        int a=1,b=1,c;
+        int limit = sc.nextInt();                //accept the limit
+        int a=1,b=1,c;                           //intializing the first 2 numbers in the fibanocci series
         System.out.println("Fibonacci sequence up to " + limit + ":");
-        while (a<=limit) {
-            System.out.print(a + " ");
-            c=a+b;
-            a=b;
-            b=c;
+        while (a<=limit) {                        
+            System.out.print(a + " ");            //printing the current element in the series
+            c=a+b;                                //calculating the next element in the series 
+            a=b;                                  // Move the current number to the previous position
+            b=c;                                  // Move the next number to the current position 
         }
     }
 }
