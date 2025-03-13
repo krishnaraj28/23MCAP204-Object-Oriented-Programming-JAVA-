a675f71b47dd375d147e6bf02153eb29287107e6
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 22:23:12 +0530

    Qn_10

--- Cycle_1/RectConstructor.java
+++ Cycle_1/RectConstructor.java
@@ -1,21 +1,21 @@
 class Rectangle {
     private double length;
     private double width;
-    public Rectangle() {
+    Rectangle() {                                   //default constructor 
         this.length = 1;
         this.width = 1;
     }
-    public Rectangle(double length, double width) {
+    Rectangle(double length, double width) {        //parameterized constructor
         this.length = length;
         this.width = width;
     }
-    public double calculateArea() {
+    public double calculateArea() {                 //method for calculate area
         return length * width;
     }
-    public double calculatePerimeter() {
+    public double calculatePerimeter() {            //method for calculate perimeter
         return 2 * (length + width);
     }
-    public void displayInfo() {
+    public void displayInfo() {                     //method for display details
         System.out.println("Rectangle:");
         System.out.println("Length: " + length);
         System.out.println("Width: " + width);
@@ -24,12 +24,12 @@ class Rectangle {
         System.out.println();
     }
 }
-public class RectConstructor{
+public class RectConstructor{                       //main class
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle();
+        Rectangle rect1 = new Rectangle();          //Rectangle class object creation-default constructor
         System.out.println("Rectangle 1 (Default Constructor):");
         rect1.displayInfo();
-        Rectangle rect2 = new Rectangle(5.0, 3.5);
+        Rectangle rect2 = new Rectangle(7.0,5.5);    //Rectangle class object creation-parameterized constructor       
         System.out.println("Rectangle 2 (Parameterized Constructor):");
         rect2.displayInfo();
     }
