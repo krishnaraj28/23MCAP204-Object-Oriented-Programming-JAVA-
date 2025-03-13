1fc6f8251d6e5bf08f218ae40c93602d46f06401
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 22:15:08 +0530

    Qn_9

--- Cycle_1/Mark.java
+++ Cycle_1/Mark.java
@@ -1,13 +1,13 @@
-class Student {
+class Student {                                                 //student class
     int studentId; 
     String name;
     double marks;
-    public Student(int studentId, String name, double marks) {
+    Student(int studentId, String name, double marks) {         //constuctor for student class
         this.studentId = studentId;
         this.name = name;
         this.marks = marks;
     }
-    public void displayDetails() {
+    public void displayDetails() {                             //display method for displaying student details
         System.out.println("Student ID: " + studentId);
         System.out.println("Name: " + name);
         System.out.println("Marks: " + marks);
@@ -18,13 +18,13 @@ class Student {
         }
         System.out.println();
     }
-    public boolean isPassed() {
+    public boolean isPassed() {                              //boolean method to determine passed or not                 
         return marks >= 40;
     }
 }
-public class Mark{
+public class Mark{                                          //main class
     public static void main(String[] args) {
-        Student s1 = new Student(101, "Anu", 50);
+        Student s1 = new Student(101, "Anu", 50);           //creating class object   
         Student s2 = new Student(102, "Manu", 40);
         Student s3 = new Student(103, "Arun", 39);
         s1.displayDetails();
