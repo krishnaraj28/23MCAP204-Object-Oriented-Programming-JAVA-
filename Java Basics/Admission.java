c28be2f6028ba3727db8ef103bfb79dcaa18e0c3
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:36:28 +0530

    Qn_3
     write a program to find eligibility of a student :
     	a) Input marks for Math, Physics, and Chemistry.
     	b) Check eligibility based on:
     		Total marks of Math, Physics, and Chemistry.
    1. Math >= 60 and physics >=50 and chemistry>=40 and total >=200
    2. Total marks of Math and Physics >= 150

--- Java Basics/Admission.java
+++ Java Basics/Admission.java
@@ -11,9 +11,9 @@ public class Admission {
 		float chemistry=sc.nextFloat();
 		double total=maths+physics+chemistry;	//total mark calculation
 		double mapy=maths+physics;
-		System.out.println("TOTAL:"+total); //total mark
+		System.out.println("TOTAL:"+total); 	//total mark
 		System.out.println("TOTAL(Maths+Physics):"+mapy); //mark of physics+maths
-		if(maths>=60 && physics>=50 && chemistry>=40 &&total>=200||mapy>=150) { //eligible candidate checking usingg if-else
+		if(maths>=60 && physics>=50 && chemistry>=40 &&total>=200||mapy>=150) { 	//eligible candidate checking usingg if-else
 			System.out.println("Eligible Candiate");	
 		}
 		else {
