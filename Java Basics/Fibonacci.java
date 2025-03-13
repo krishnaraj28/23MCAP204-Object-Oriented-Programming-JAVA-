fe8ce5a0510fda66147b0e4620cf8c8f833c84f2
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:34:12 +0530

    Qn_8
    Print the Fibonacci sequence up to a given limit.

--- Java Basics/Fibonacci.java
+++ Java Basics/Fibonacci.java
@@ -3,8 +3,8 @@ public class Fibonacci{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the limit: ");
-        int limit = sc.nextInt();                //accept the limit
-        int a=1,b=1,c;                           //intializing the first 2 numbers in the fibanocci series
+        int limit = sc.nextInt();                 //accept the limit
+        int a=1,b=1,c;                            //intializing the first 2 numbers in the fibanocci series
         System.out.println("Fibonacci sequence up to " + limit + ":");
         while (a<=limit) {                        
             System.out.print(a + " ");            //printing the current element in the series
