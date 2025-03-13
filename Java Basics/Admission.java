280bd7af8a66d6061cb8899e329730cff5aabda8
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 18:48:39 +0530

    Update Admission.java

--- Java Basics/Admission.java
+++ Java Basics/Admission.java
@@ -3,7 +3,7 @@ public class Admission {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Mark of Math:");
+		System.out.println("Enter the Mark of Math:");	//input from user
 		float maths=sc.nextFloat();
 		System.out.println("Enter the Mark of Physics:");
 		float physics=sc.nextFloat();
@@ -12,10 +12,9 @@ public class Admission {
 		double total=maths+physics+chemistry;
 		double mapy=maths+physics;
 		System.out.println("TOTAL:"+total); //total mark
-		System.out.println("TOTAL(Maths+Physics):"+mapy); //mark of physics and maths
-
-		if(maths>=60 && physics>=50 && chemistry>=40 &&total>=200||mapy>=150) {
-			System.out.println("Eligible Candiate");
+		System.out.println("TOTAL(Maths+Physics):"+mapy); //mark of physics+maths
+		if(maths>=60 && physics>=50 && chemistry>=40 &&total>=200||mapy>=150) { //eligible candidate checking usingg if-else
+			System.out.println("Eligible Candiate");	
 		}
 		else {
 			System.out.println("Not Eligible");
