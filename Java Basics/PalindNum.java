6585c6f1b041c8cc0e4c5d25d230ec8988b46445
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:49:46 +0530

    Qn_6
    Write a program to check if a number is a palindrome.

--- Java Basics/PalindNum.java
+++ Java Basics/PalindNum.java
@@ -3,17 +3,17 @@ public class PalindNum{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        int originalNum = num;
-        int reversedNum = 0;
+        int num = sc.nextInt();                        //reading  the number from user
+        int originalNum = num;                         //Assign the orginal number to the variable 
+        int reversedNum = 0;                           //intialize a variablle to store the reversed number 
 
-        while (num > 0) {
-            int digit = num % 10;
-            reversedNum = reversedNum * 10 + digit;
-            num /= 10;
+        while (num > 0) {                              //Continue until the number is less than 0
+            int digit = num % 10;                      //extarct the last digit
+            reversedNum = reversedNum * 10 + digit;    //add that to the the variable that store the reversed number 
+            num /= 10;                                 //remove the last digit
         }
 
-        if (originalNum == reversedNum) {
+        if (originalNum == reversedNum) {            //check if revesed and orginal number is same
             System.out.println(originalNum + " is a palindrome.");
         } else {
             System.out.println(originalNum + " is not a palindrome.");
