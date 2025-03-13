39a4a919d7ce59e5e3325897390b2b115b999b92
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:33:18 +0530

    Qn_1
     Write a program to find the area and perimeter of a circle.

--- Java Basics/Circle.java
+++ Java Basics/Circle.java
@@ -5,8 +5,8 @@ public class Circle{
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the Radius:");
 		float a = s.nextFloat();
-		double area=3.14*a*a;	//calculating area 
-		double peri=2*3.14*a;	//calculated the perimeter
+		double area=3.14*a*a;	 //calculating area 
+		double peri=2*3.14*a;	 //calculated the perimeter
 		System.out.println("Area:"+area);	
 		System.out.println("Perimeter:"+peri);
 	}
