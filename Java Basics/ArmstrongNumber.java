0c9e2deecaddc6052d418c850e37f5963bcbfc4c
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 19:52:55 +0530

    Qn_7
    Write a program to check if a number is an Armstrong number.

--- Java Basics/ArmstrongNumber.java
+++ Java Basics/ArmstrongNumber.java
@@ -6,13 +6,13 @@ public class ArmstrongNumber {
         int num=sc.nextInt();
         int original=num,sum=0,digits=0,remainder;
         int temp=num;
-        while(temp!=0) {    //counting the digits of the accepted number
+        while(temp!=0) {      //counting the digits of the accepted number
             temp/=10;
             digits++;        //increment the digit
         }
-        temp=num;    //intialize temp=num
+        temp=num;            //intialize temp=num
         while(temp!=0) {            
-            remainder=temp % 10;                  //extarcting each number for calculating the power
+            remainder=temp % 10;                  //extracting each number for calculating the power
             sum+=Math.pow(remainder, digits);    //calculating the sum of the power of each digit 
             temp/=10;                            //removing the extracted digit
         }
