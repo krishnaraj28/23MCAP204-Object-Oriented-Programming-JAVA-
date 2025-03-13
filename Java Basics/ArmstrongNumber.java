41964540f3a196d35f3980fa0f302d70c261163b
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-13 18:54:31 +0530

    Update ArmstrongNumber.java

--- Java Basics/ArmstrongNumber.java
+++ Java Basics/ArmstrongNumber.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number: ");     //take a number as user input
         int num=sc.nextInt();
         int original=num,sum=0,digits=0,remainder;
         int temp=num;
-        while(temp!=0) {
+        while(temp!=0) {    //counting the digits of the accepted number
             temp/=10;
-            digits++;
+            digits++;        //increment the digit
         }
-        temp=num;
-        while(temp!=0) {
-            remainder=temp % 10;
-            sum+=Math.pow(remainder, digits);
-            temp/=10;
+        temp=num;    //intialize temp=num
+        while(temp!=0) {            
+            remainder=temp % 10;                  //extarcting each number for calculating the power
+            sum+=Math.pow(remainder, digits);    //calculating the sum of the power of each digit 
+            temp/=10;                            //removing the extracted digit
         }
         if(sum==num) {
             System.out.println("The number is an Armstrong number.");
