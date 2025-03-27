5f7e9e18f63878c598be4fffcb7cf66475a4a16a
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-27 10:48:09 +0530

    17

--- Cycle-2/CountVC.java
+++ Cycle-2/CountVC.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class CountVC{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a line of text: ");
+        System.out.print("Enter a line of text: ");     //inputing the string
         String text = sc.nextLine();
-        text = text.toLowerCase();
-        int vowels = 0, consonants = 0;
-        for (int i = 0; i < text.length(); i++) {
+        text = text.toLowerCase();                      //make the string to lowercase
+        int vowels = 0, consonants = 0;                 //intialize vowels and consonants
+        for (int i = 0; i < text.length(); i++) {       //scan through the string for counting the vowels & consonants
             char ch = text.charAt(i);
             if (ch >= 'a' && ch <= 'z') {
                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
