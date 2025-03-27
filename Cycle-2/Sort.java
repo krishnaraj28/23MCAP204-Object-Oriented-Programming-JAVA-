1c7162d39ded924bacd7812c33d96aecb21e47fc
Krishna Raj <134435199+krishnaraj28@users.noreply.github.com>
2025-03-27 10:50:47 +0530

    16

--- Cycle-2/Sort.java
+++ Cycle-2/Sort.java
@@ -6,20 +6,20 @@ public class Sort{
         int n = sc.nextInt();
         sc.nextLine();
         String[] arr = new String[n];
-        System.out.println("Enter the strings:");
+        System.out.println("Enter the strings:");           //get the input
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextLine();
+            arr[i] = sc.nextLine();                         //store the in a array
         }
-        for (int i=0;i<n-1; i++) {
+        for (int i=0;i<n-1; i++) {                      
             for (int j=0;j<n-1-i;j++) {
-                if (arr[j].compareToIgnoreCase(arr[j + 1]) > 0) {
+                if (arr[j].compareToIgnoreCase(arr[j + 1]) > 0) {   //check the lexicographic order
                     String temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
-        System.out.println("Sorted strings:");
+        System.out.println("Sorted strings:");              //print the sorted array
         for (int i=0;i<arr.length; i++) {
             System.out.println(arr[i]);
         }
